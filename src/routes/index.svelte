655239c720b0486e86e201234aeaeafab2f3f9cd
Luke Hagar <lukeslakemail@gmail.com>
2022-08-04 23:06:54 -0500

    Functional TailWind CSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {
+    Tab,
+    TabGroup,
+    TabList,
+    TabPanel,
+    TabPanels,
+  } from "@rgossiaux/svelte-headlessui";
+</script>
+
+<TabGroup>
+  <TabList>
+    <Tab class="bg-sky-500 w-32 h-20 shadow-lg rounded-full">Tab 1</Tab>
+    <Tab class="bg-sky-500 w-32 h-20 shadow-lg rounded-full">Tab 2</Tab>
+    <Tab class="bg-sky-500 w-32 h-20 shadow-lg rounded-full ">Tab 3</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel class="bg-sky-500 w-32 h-20">Content 1</TabPanel>
+    <TabPanel>Content 2</TabPanel>
+    <TabPanel>Content 3</TabPanel>
+  </TabPanels>
+</TabGroup>
