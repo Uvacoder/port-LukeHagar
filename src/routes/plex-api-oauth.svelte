be40674382d13142c0fdf179d3628de430f374ad
Luke Hagar <lukeslakemail@gmail.com>
2022-08-06 21:56:05 -0500

    adding Project page for plex-api-oauth

---
+++ src/routes/plex-api-oauth.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Footer from "../components/Footer.svelte";
+  import NavBar from "../components/NavBar.svelte";
+</script>
+
+<svelte:head>
+  <title>plex-api-oauth</title>
+</svelte:head>
+
+<div class=" bg-slate-500 h-screen">
+  <NavBar currentPage="plex-api-oauth" />
+
+  <div class="px-2 py-2" />
+  <Footer />
+</div>
