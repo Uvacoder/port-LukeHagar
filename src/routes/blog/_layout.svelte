99e492b3d72c1ff249e81a924620b8a872ce896d
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-08 22:07:06 -0500

    Code fence

--- src/routes/blog/_layout.svelte
+++ src/routes/blog/_layout.svelte
@@ -7,11 +7,11 @@
   export let date;
 </script>
 
-<NavBar currentPage={title} />
+<NavBar currentPage="Blog" />
 <Body>
   <div class="py-2">
     <div class="grid grid-cols-2 flex-row bg-white px-2 py-2 rounded-xl my-2">
-      <div class="flex flex-col justify-start">
+      <div class="flex flex-col justify-start px-2">
         <p class="text-xl">{title}</p>
         <p class="text-xs">By: {author}</p>
       </div>
@@ -19,6 +19,8 @@
         <p class="text-xl py-2 px-2">On: {date}</p>
       </div>
     </div>
-    <slot />
+    <div class="px-2">
+      <slot />
+    </div>
   </div>
 </Body>
--- src/routes/blog/posts.svelte
+++ src/routes/blog/posts.svelte
@@ -7,10 +7,13 @@
 
 <NavBar currentPage="Blog" />
 <Body>
-  <div class="grid grid-cols-4 gap-3">
+  <div class="px-4 py-4">
+    <h1>Posts:</h1>
+  </div>
+  <div class="grid grid-cols-4 gap-4 py-2 px-2 my-4">
     {#each posts as post}
       <a href={post.path}>
-        <div class="rounded-xl py-2 px-2 mx-2 my-2 bg-white">
+        <div class="rounded-xl py-2 px-2 my-4 bg-white">
           <p class="text-xl">{post.title}</p>
           <p class="text-xs">{post.date}</p>
         </div>
