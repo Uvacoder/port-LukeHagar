230558d130d543eadd8a6d132183f9316a8088aa
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-20 21:49:32 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,11 +82,12 @@
       <a
         href="https://www.github.com/lukehagar"
         target="_blank"
-        class="min-h-fit content-center"
+        class="content-center"
       >
         <img
           alt="My Personal GitHub Stats"
           href="https://www.github.com/lukehagar"
+          class="min-h-fit"
           src="https://raw.githubusercontent.com/lukehagar/github-stats/master/generated/languages.svg#gh-dark-mode-only"
         />
       </a>
