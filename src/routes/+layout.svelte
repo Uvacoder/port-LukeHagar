c2b8a66aaa79ec8118140f8659e9165e78f19362
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-23 08:23:17 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -80,6 +80,18 @@
     for (const i of Array(1000).fill()) {
       addStar();
     }
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
   });
 
   export let data;
@@ -214,8 +226,10 @@
 
 <style>
   @supports (-webkit-touch-callout: none) {
-    .ios-h-screen {
+    .fullscreen {
       height: -webkit-fill-available;
+      height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+      height: calc(var(--vh, 1vh) * 100);
     }
   }
 </style>
